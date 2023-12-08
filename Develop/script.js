e9f822453e3d947f5ba987f4e9e786e8d8461072
maymikb <123998301+maymikb@users.noreply.github.com>
2023-12-08 15:23:29 -0800

    readMe,html,gitignore added

--- Develop/script.js
+++ Develop/script.js
@@ -23,8 +23,12 @@ $(function () {
     localStorage.setItem(parentId, textToSave)
   })
 
-
-
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time? FOR LOOP?
   var currentHour = dayjs().hour()
    console.log(currentHour)
 
@@ -45,27 +49,20 @@ $(function () {
         this.classList.add("future")
       }
  })
-
-
-
-  buttons.each(function(){
-    var parentId=this.parentElementId;
-    var savedtext=localStorage.getItem()
-    this.previousElementSibling.value= savedText
-  })
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time? FOR LOOP?
    
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
+  //console.log(buttons)
+  
+  buttons.each(function(){
+    var parentId = this.parentElement.id;
+    var savedtext = localStorage.getItem(parentId)
+    this.previousElementSibling.value= savedtext
+  })
+
   // TODO: Add code to display the current date in the header of the page.
    var currentDate = dayjs ().format("MMMM DD, YYYY")
   $("#currentDay").text(currentDate)
